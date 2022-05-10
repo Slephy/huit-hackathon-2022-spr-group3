da44a43c37da6d47dccf7ac979bc774c7ed4f737
Slephy <52317011+Slephy@users.noreply.github.com>
2022-05-10 14:04:08 +0900

    commit

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def handle_message(event):
     msg = f"「{event.message.text}」ですか？ ちょっとよくわかりませんね…"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
-    msg = "「これは位置情報ですか？ ちょっとよくわかりませんね…"
+    msg = "これは位置情報ですか？ ちょっとよくわかりませんね…"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
 
 
