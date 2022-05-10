220cc7cb5aeec836feef7a67865f77b066e029d5
Slephy <52317011+Slephy@users.noreply.github.com>
2022-05-10 14:01:27 +0900

    commit

--- app.py
+++ app.py
@@ -11,55 +11,12 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# # じゃんけん処理に使用
-# DRAW = 0
-# USER1_WIN = 1
-# USER2_WIN = 2
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-# # 1 -> グー , 2 -> チョキ, 3 -> パー
-# def game_janken(user1, user2):
-#     # IDで受け取る。
-#     if user1 not in [1, 2, 3] or user2 not in [1, 2, 3]:
-#         raise Exception("game_janken() failure: invalid params")
-
-#     if user1 == user2:
-#         return DRAW
-#     elif (
-#         (user1 == 1 and user2 == 2)
-#         or (user1 == 2 and user2 == 3)
-#         or (user1 == 3 and user2 == 1)
-#     ):
-#         return USER1_WIN
-#     else:
-#         return USER2_WIN
-
-
-# def hand_id2str(hand_id):
-#     if hand_id == 1:
-#         return "ぐー"
-#     elif hand_id == 2:
-#         return "ちょき"
-#     elif hand_id == 3:
-#         return "ぱー"
-
-#     raise Exception("handid2str() failure: 不正な入力値です")
-
-
-# def hand_str2id(hand_str):
-#     if hand_str == "ぐー":
-#         return 1
-#     elif hand_str == "ちょき":
-#         return 2
-#     elif hand_str == "ぱー":
-#         return 3
-
-#     raise Exception("hand_str2id() failure: 不正な入力値です")
 
 
 @app.route("/callback", methods=["POST"])
