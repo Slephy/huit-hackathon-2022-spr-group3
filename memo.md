f56204bd429c31fe11ce80d15c3dc4425c63b5fa
Toyoshin <43494392+toyoshin5@users.noreply.github.com>
2022-05-12 11:59:36 +0900

    getInfoOf3routes

--- memo.md
+++ memo.md
@@ -3,3 +3,22 @@
 - 複数個応答メッセージを返したいときには、reply_messageの引数で渡すmessageにSendTextMessageクラスのタプルや配列を指定する。単に複数回reply_messageを呼び出しても最初の1回しか処理してくれない
 
 - Yahoo乗換案内を使用
+
+- soup.select()でCSSセレクタによる指定が可能
+
+## 経由地がある状況においてのCSSセレクタによる時刻取得
+電車->電車の場合
+乗車時刻->降車時刻(乗り換え)->乗車時刻(乗り換え)->降車時刻
+```
+#route01 > div > div:nth-child(1) > ul.time > li
+#route01 > div > div.fareSection > div.station > ul > li:nth-child(1)
+#route01 > div > div:nth-child(3) > ul.time > li
+```
+電車->徒歩の場合
+乗車時刻->降車時刻->徒歩出発時刻->徒歩到着時刻 の場合
+```
+#route01 > div > div:nth-child(1) > ul.time > li
+#route01 > div > div:nth-child(3) > ul > li:nth-child(1)
+#route01 > div > div:nth-child(3) > ul > li:nth-child(2)
+#route01 > div > div:nth-child(3) > ul.time > li
+```
--- webscrapingtest.py
+++ webscrapingtest.py
@@ -6,29 +6,51 @@ import datetime
 import requests
 from bs4 import BeautifulSoup
 
-departure_station = "北18条"
-destination_station = "恵庭"
-
+departure_station = "札幌"
+destination_station = "苗穂"
 # 経路の取得先URL
-route_url = "https://transit.yahoo.co.jp/search/print?from=" + \
-    departure_station+"&flatlon=&to=" + destination_station
+urla = 'https://transit.yahoo.co.jp/search/result?from='
+urlb = '&flatlon=&to='
+urlc = '&fromgid=&togid=&flatlon=&tlatlon=&via=&viacode=&y=2022&m=05&d=12&hh=10&m1=3&m2=8&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=0&shin=0&ex=1&hb=0&lb=0&sr=0'
+route_url = urla + departure_station + urlb + destination_station + urlc
+
 print(route_url)
-
 route_response = requests.get(route_url)
-
-
 route_soup = BeautifulSoup(route_response.text, 'html.parser')
+# 3つの候補の路線名を取得
+rosens = []
+a = route_soup.find_all(class_="transport")
+for i in range(1, 4):
+    txt = a[i].get_text()
+    txt = txt.replace("[line][train]", "")
+    rosens.append(txt)
+print("路線名:\n", rosens)
+# 3つの候補の出発時刻を取得
+deptime = []
+for i in range(1, 4):
+    li = str(route_soup.select(
+        "#route0"+str(i)+" > div > div:nth-child(1) > ul.time > li")[0])
+    li = li.replace("<li>", "")
+    li = li.replace("</li>", "")
+    deptime.append(li)
+# 3つの候補の到着時刻を取得
+print("出発時刻:\n", deptime)
+arrtime = []
+for i in range(1, 4):
+    li = str(route_soup.select(
+        "#route0"+str(i)+" > div > div:nth-child(3) > ul.time > li")[0])
+    li = li.replace("<li>", "")
+    li = li.replace("</li>", "")
+    arrtime.append(li)
+print("到着時刻:\n", arrtime)
+# 3つの候補の金額を取得
+prices = []
+for i in range(1, 4):
+    li = str(route_soup.select(
+        "#route0"+str(i)+" > div > div.fareSection > p > span")[0])
+    li = li.replace("<span>", "")
+    li = li.replace("</span>", "")
+    prices.append(li)
+print("金額:\n", prices)
 
-
-route_summary = route_soup.find("div", class_="routeSummary")
-
-required_time = route_summary.find("li", class_="time").get_text()
-
-transfer_count = route_summary.find("li", class_="transfer").get_text()
-
-fare = route_summary.find("li", class_="fare").get_text()
-
-print("======"+departure_station+"から"+destination_station+"=======")
-print("所要時間："+required_time)
-print(transfer_count)
-print("料金："+fare)
+# //*[@id="route02"]/div/div[2]/div/ul/li[1]/div/text()
